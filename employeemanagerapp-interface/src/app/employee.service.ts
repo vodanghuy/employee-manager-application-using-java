2afee02d541ca31c9a5e2d2375f973c3d17879e4
Vo Dang Huy <iamdaghuy@gmail.com>
2025-11-08 14:26:18 +0700

    update repo

---
+++ employeemanagerapp-interface/src/app/employee.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Employee } from './employee';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+
+ 
+}
