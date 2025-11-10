4e57b259a82750a1848c840153849f1bdf690c93
Vo Dang Huy <iamdaghuy@gmail.com>
2025-11-10 22:56:47 +0700

    search functionality

--- employeemanager-interface/src/app/app.ts
+++ employeemanager-interface/src/app/app.ts
@@ -72,6 +72,22 @@ export class App implements OnInit{
     )
   }
 
+  public onSearch(key: string): void{
+    const results: Employee[] = [];
+    for(const employee of this.employees){
+      if(employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1){
+        results.push(employee);
+      }
+    }
+    this.employees = results;
+    if(results.length === 0 || !key){
+      this.getEmployees();
+    }
+  }
+
   public onOpenModal(employee: Employee | null, mode: string): void{
       const container = document.getElementById('main-container');
       if (!container) {
